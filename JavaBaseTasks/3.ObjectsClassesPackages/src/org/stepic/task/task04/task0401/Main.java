b8fb8ca061aab1e67d5729fe277a5ab41110add7
tihonov-e <tihonov.pcb@gmail.com>
2021-07-30 08:59:51 +0300

    Приступил к разделу про исключения. Решил задачу про sqrt() 4.1 Знакомство с исключениями

---
+++ JavaBaseTasks/3.ObjectsClassesPackages/src/org/stepic/task/task04/task0401/Main.java
@@ -0,0 +1,9 @@
+package org.stepic.task.task04.task0401;
+
+public class Main {
+    public static void main(String[] args) {
+        Object x = new Integer(0);
+        System.out.println((String) x);
+    }
+
+}
